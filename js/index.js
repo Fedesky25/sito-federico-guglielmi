6d8203445f554b3992565cf3610312357042e0ad
Fedesky25 <fedesky.guglielmi@gmail.com>
2021-06-27 17:36:01 +0200

    smooth scroll

---
+++ js/index.js
@@ -0,0 +1,25 @@
+/**
+* Smoothly scrolls to the target
+* @param {HTMLElement} target target of the scrolling
+* @param {Number} offset offset to apply
+* @param {Number} duration duration of the scrolling
+*/
+function smoothScrollTo(target, offset, duration) {
+    let start = window.scrollY, end = target.offsetTop - offset, delta, initialTime;
+    if (end > document.body.scrollHeight - window.innerHeight) delta = document.body.scrollHeight - window.innerHeight - start;
+    else if (end < 0) delta = - start; else delta = end - start;
+    function animate(timeStamp) {
+        if (initialTime === undefined) initialTime = timeStamp;
+        const timeElapsed = timeStamp-initialTime;
+        window.scrollTo(0, quadInOut(timeElapsed, start, delta, duration));
+        if (timeElapsed < duration) window.requestAnimationFrame(animate);
+    }
+    window.requestAnimationFrame(animate)
+}
+function quadInOut (t, b, c, d) {t /= d/2; if (t < 1) return c/2*t*t + b; t--; return -c/2 * (t*(t-2) - 1) + b;};
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+    const target = document.getElementById(anchor.getAttribute('href').slice(1));
+    if(!target) return;
+    anchor.addEventListener('click', e=>{e.preventDefault(); smoothScrollTo(target, 50, 350); anchor.blur();})
+});

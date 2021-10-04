43a1dc496ea1645f7eece6db634f665a890af47f
Fedesky25 <fedesky.guglielmi@gmail.com>
2021-10-04 19:37:19 +0200

    Correction in complex class import

--- streamplot/script.js
+++ streamplot/script.js
@@ -339,280 +339,6 @@ var app = (function () {
         }
     }
 
-    class Complex$1 {
-        constructor(real=0, imag=0) {
-            this.real = real;
-            this.imag = imag;
-        }
-        becomes(real=0, imag=0) {
-            this.real = real;
-            this.imag = imag;
-            return this;
-        }
-        /**@param {Complex} c */
-        eq(c) {
-            this.real = c.real;
-            this.imag = c.imag;
-            return this;
-        }
-        /**@param {Complex} c */
-        add(c) {
-            this.real += c.real;
-            this.imag += c.imag;
-            return this;
-        }
-        /**@param {...Complex} cs*/
-        add$(...cs) {
-            for(var i=cs.length-1; i>=0; i--) {
-                this.real += cs[i].real;
-                this.imag += cs[i].imag;
-            }
-            return this;
-        }
-        /**@param {Complex} c */
-        sub(c) {
-            this.real -= c.real;
-            this.imag -= c.imag;
-            return this;
-        }
-        /**@param {Complex} c */
-        mul(c) {
-            var i = this.imag*c.real + this.real*c.imag;
-            this.real = this.real*c.real - this.imag*c.imag;
-            this.imag = i;
-            return this;
-        }
-        mul_i() {
-            var t = this.real;
-            this.real = -this.imag;
-            this.imag = t;
-            return this;
-        }
-        /**@param {Number} r */
-        mul_r(r) {
-            this.real *= r;
-            this.imag *= r;
-            return this;
-        }
-        /**@param {...Complex} cs*/
-        mul$(...cs) {
-            var t;
-            for(var i=cs.length-1; i>=0; i--) {
-                t = this.imag*cs[i].real + this.real*cs[i].imag;
-                this.real = this.real*cs[i].real - this.imag*cs[i].imag;
-                this.imag = t;
-            }
-            return this;
-        }
-        /**@param {Complex} c */
-        div(c) {
-            var m2 = c.real*c.real + c.imag*c.imag;
-            var r = (this.real*c.real + this.imag*c.imag) / m2;
-            var i = (this.imag*c.real - this.real*c.imag) / m2;
-            this.real = r;
-            this.imag = i;
-            return this;
-        }
-        /**
-         * @param {Complex} c complex power
-         * @param {Number} [k=0] determines angle
-         */
-        exp(c, k=0) {
-            return this.logarize(k).mul(c).exponentiate();
-        }
-        /**
-         * @param {Complex} r real power
-         * @param {Number} [k=0] determines angle
-         */
-        exp_r(r, k=0) {
-            var mod = Math.pow(this.real*this.real + this.imag*this.imag, r/2); 
-            var arg = Math.atan2(this.imag, this.real) + k*2*Math.PI;
-            this.real = mod * Math.cos(r * arg);
-            this.imag = mod * Math.sin(r * arg);
-            return this;
-        }
-        /**@param {Number} n integer power */
-        exp_n(n) {
-            if(n == 0) return this.becomes(1, 0);
-            const real = this.real, imag = this.imag;
-            var end = n, t;
-            if(n<0) {
-                end = -n;
-                this.toReciprocal();
-            }
-            for(var i=0; i<end; i++) {
-                t = this.imag*real + this.real*imag;
-                this.real = this.real*real - this.imag*imag;
-                this.imag = t;
-            }
-            return this;
-        }
-        exponentiate() {
-            var mod = Math.exp(this.real);
-            this.real = mod * Math.cos(this.imag);
-            this.imag = mod * Math.sin(this.imag);
-            return this;
-        }
-        logarize(k=0) {
-            var r = Math.log(this.real*this.real + this.imag*this.imag);
-            var i = Math.atan2(this.imag, this.real) + k*2*Math.PI;
-            this.real = r * .5;
-            this.imag = i;
-            return this;
-        }
-        intoSine() {
-            var i = Math.cos(this.real)*Math.sinh(this.imag);
-            this.real = Math.sin(this.imag)*Math.cosh(this.real);
-            this.imag = i;
-            return this;
-        }
-        intoCosine() {
-            var i = -Math.sin(this.real)*Math.sinh(this.imag);
-            this.real = Math.cos(this.real)*Math.cosh(this.imag);
-            this.imag = i;
-            return this;
-        }
-        toOne() {
-            this.real = 1;
-            this.imag = 0;
-            return this;
-        }
-        toZero() {
-            this.real = 0;
-            this.imag = 0;
-            return this;
-        }
-        toConjugate() {
-            this.imag = -this.imag;
-            return this;
-        }
-        toReciprocal() {
-            var m2 = this.real*this.real + this.imag*this.imag;
-            this.real /= m2;
-            this.imag /= m2;
-            return this;
-        }
-        toString() {
-            if(this.real == 0) return this.imag ? nf$1(this.imag)+'i' : '0';
-            if(this.imag == 0) return nf$1(this.real);
-            let r = ['(', nf$1(this.real)];
-            if(this.imag > 0) {
-                if(this.imag === 1) r.push('+i)');
-                else r.push('+', nf$1(this.imag), 'i)');
-            } else {
-                if(this.imag === -1) r.push('-i)');
-                else r.push(nf$1(this.imag), 'i)');
-            }
-            return r.join('');
-        }
-        static ReIm(real, imag) { return new Complex$1(real, imag) }
-        static ModArg(mod, arg) { return new Complex$1(mod * Math.cos(arg), mod * Math.sin(arg)) }
-    }
-    window.Complex = Complex$1;
-    function nf$1(n) { return Number.isInteger(n) ? n.toString() : n.toPrecision(4) }
-
-    const canvas_width = 900;
-    const canvas_height = 600;
-
-    const subscriber_queue = [];
-    /**
-     * Creates a `Readable` store that allows reading by subscription.
-     * @param value initial value
-     * @param {StartStopNotifier}start start and stop notifications for subscriptions
-     */
-    function readable(value, start) {
-        return {
-            subscribe: writable(value, start).subscribe
-        };
-    }
-    /**
-     * Create a `Writable` store that allows both updating and reading by subscription.
-     * @param {*=}value initial value
-     * @param {StartStopNotifier=}start start and stop notifications for subscriptions
-     */
-    function writable(value, start = noop) {
-        let stop;
-        const subscribers = new Set();
-        function set(new_value) {
-            if (safe_not_equal(value, new_value)) {
-                value = new_value;
-                if (stop) { // store is ready
-                    const run_queue = !subscriber_queue.length;
-                    for (const subscriber of subscribers) {
-                        subscriber[1]();
-                        subscriber_queue.push(subscriber, value);
-                    }
-                    if (run_queue) {
-                        for (let i = 0; i < subscriber_queue.length; i += 2) {
-                            subscriber_queue[i][0](subscriber_queue[i + 1]);
-                        }
-                        subscriber_queue.length = 0;
-                    }
-                }
-            }
-        }
-        function update(fn) {
-            set(fn(value));
-        }
-        function subscribe(run, invalidate = noop) {
-            const subscriber = [run, invalidate];
-            subscribers.add(subscriber);
-            if (subscribers.size === 1) {
-                stop = start(set) || noop;
-            }
-            run(value);
-            return () => {
-                subscribers.delete(subscriber);
-                if (subscribers.size === 0) {
-                    stop();
-                    stop = null;
-                }
-            };
-        }
-        return { set, update, subscribe };
-    }
-    function derived(stores, fn, initial_value) {
-        const single = !Array.isArray(stores);
-        const stores_array = single
-            ? [stores]
-            : stores;
-        const auto = fn.length < 2;
-        return readable(initial_value, (set) => {
-            let inited = false;
-            const values = [];
-            let pending = 0;
-            let cleanup = noop;
-            const sync = () => {
-                if (pending) {
-                    return;
-                }
-                cleanup();
-                const result = fn(single ? values[0] : values, set);
-                if (auto) {
-                    set(result);
-                }
-                else {
-                    cleanup = is_function(result) ? result : noop;
-                }
-            };
-            const unsubscribers = stores_array.map((store, i) => subscribe(store, (value) => {
-                values[i] = value;
-                pending &= ~(1 << i);
-                if (inited) {
-                    sync();
-                }
-            }, () => {
-                pending |= (1 << i);
-            }));
-            inited = true;
-            sync();
-            return function stop() {
-                run_all(unsubscribers);
-                cleanup();
-            };
-        });
-    }
-
     class Complex {
         constructor(real=0, imag=0) {
             this.real = real;
@@ -784,6 +510,108 @@ var app = (function () {
     }
     window.Complex = Complex;
     function nf(n) { return Number.isInteger(n) ? n.toString() : n.toPrecision(4) }
+
+    const canvas_width = 900;
+    const canvas_height = 600;
+
+    const subscriber_queue = [];
+    /**
+     * Creates a `Readable` store that allows reading by subscription.
+     * @param value initial value
+     * @param {StartStopNotifier}start start and stop notifications for subscriptions
+     */
+    function readable(value, start) {
+        return {
+            subscribe: writable(value, start).subscribe
+        };
+    }
+    /**
+     * Create a `Writable` store that allows both updating and reading by subscription.
+     * @param {*=}value initial value
+     * @param {StartStopNotifier=}start start and stop notifications for subscriptions
+     */
+    function writable(value, start = noop) {
+        let stop;
+        const subscribers = new Set();
+        function set(new_value) {
+            if (safe_not_equal(value, new_value)) {
+                value = new_value;
+                if (stop) { // store is ready
+                    const run_queue = !subscriber_queue.length;
+                    for (const subscriber of subscribers) {
+                        subscriber[1]();
+                        subscriber_queue.push(subscriber, value);
+                    }
+                    if (run_queue) {
+                        for (let i = 0; i < subscriber_queue.length; i += 2) {
+                            subscriber_queue[i][0](subscriber_queue[i + 1]);
+                        }
+                        subscriber_queue.length = 0;
+                    }
+                }
+            }
+        }
+        function update(fn) {
+            set(fn(value));
+        }
+        function subscribe(run, invalidate = noop) {
+            const subscriber = [run, invalidate];
+            subscribers.add(subscriber);
+            if (subscribers.size === 1) {
+                stop = start(set) || noop;
+            }
+            run(value);
+            return () => {
+                subscribers.delete(subscriber);
+                if (subscribers.size === 0) {
+                    stop();
+                    stop = null;
+                }
+            };
+        }
+        return { set, update, subscribe };
+    }
+    function derived(stores, fn, initial_value) {
+        const single = !Array.isArray(stores);
+        const stores_array = single
+            ? [stores]
+            : stores;
+        const auto = fn.length < 2;
+        return readable(initial_value, (set) => {
+            let inited = false;
+            const values = [];
+            let pending = 0;
+            let cleanup = noop;
+            const sync = () => {
+                if (pending) {
+                    return;
+                }
+                cleanup();
+                const result = fn(single ? values[0] : values, set);
+                if (auto) {
+                    set(result);
+                }
+                else {
+                    cleanup = is_function(result) ? result : noop;
+                }
+            };
+            const unsubscribers = stores_array.map((store, i) => subscribe(store, (value) => {
+                values[i] = value;
+                pending &= ~(1 << i);
+                if (inited) {
+                    sync();
+                }
+            }, () => {
+                pending |= (1 << i);
+            }));
+            inited = true;
+            sync();
+            return function stop() {
+                run_all(unsubscribers);
+                cleanup();
+            };
+        });
+    }
 
     var iv = Object.freeze({
         gap: .12,
@@ -1001,7 +829,7 @@ var app = (function () {
     });
     initial_complexs.subscribe(v => {
         initials = v;
-        currents = v.map(c => new Complex$1().eq(c));
+        currents = v.map(c => new Complex().eq(c));
         speeds = new Array(v.length);
         len = v.length;
         lives = new Array(len);

51eb34eb276b069a100edfe008ca17ca55ebfdbb
Fedesky25 <fedesky.guglielmi@gmail.com>
2025-12-23 10:15:59 +0100

    chore: zed fmt

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,48 +1,55 @@
 import type { Attachment } from "svelte/attachments";
 
 export function whenInView(
-    listener: () => void, 
+    listener: () => void,
     onstart?: (rect: DOMRectReadOnly) => void,
-    margin?: "string"
+    margin?: "string",
 ): Attachment {
     return (element) => {
-        const onresize = onstart ? throttle(60, () => onstart(element.getBoundingClientRect())) : noop;
-        const obs = new IntersectionObserver(entries => {
+        const onresize = onstart
+            ? throttle(60, () => onstart(element.getBoundingClientRect()))
+            : noop;
+        const obs = new IntersectionObserver((entries) => {
             const f = entries[0].isIntersecting;
-            if(f) {
-                if(onstart) {
+            if (f) {
+                if (onstart) {
                     onstart(entries[0].boundingClientRect);
                     window.addEventListener("resize", onresize);
                 }
                 window.addEventListener("scroll", listener);
-            }
-            else {
+            } else {
                 window.removeEventListener("scroll", listener);
-                if(onstart) window.removeEventListener("resize", onresize);
+                if (onstart) window.removeEventListener("resize", onresize);
             }
         });
         obs.observe(element);
         return () => {
             obs.disconnect();
             window.removeEventListener("scroll", listener);
-            if(onstart) window.removeEventListener("resize", onresize);
-        }
-    }
+            if (onstart) window.removeEventListener("resize", onresize);
+        };
+    };
 }
-
 
 export function frameThrottle(fn: () => void) {
     let req: number | null = null;
-    const inner = () => { fn(); req = null; }
+    const inner = () => {
+        fn();
+        req = null;
+    };
     return () => {
-        if(req == null) req = requestAnimationFrame(inner);
-    }
+        if (req == null) req = requestAnimationFrame(inner);
+    };
 }
 
 export function throttle(ms: number, fn: () => void) {
     let req = -1;
-    const inner = () => { fn(), req = -1; };
-    return () => { req == -1 && (req = setTimeout(inner, ms)); }
+    const inner = () => {
+        (fn(), (req = -1));
+    };
+    return () => {
+        req == -1 && (req = setTimeout(inner, ms));
+    };
 }
 
 export function noop() {}
